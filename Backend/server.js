4e5322ccf75aa8aac4d6f612029e09583e29e235
snsolomar <snsolomar@gmail.com>
2023-08-03 18:07:03 -0500

    dependencies and server.js init

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+
+    res.send('hello world')
+
+});
+
+app.listen(port, () => {
+
+console.log(`listening on port ${port}`)
+
+});
