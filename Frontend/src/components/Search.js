6ef00dfaec897642987a4f55d681dd1eba1252e8
snsolomar <snsolomar@gmail.com>
2023-08-04 15:07:51 -0500

    Search Component started, making new branch

---
+++ Frontend/src/components/Search.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const Search = () => {
+    const [input, setInput] = useState("")
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Search
