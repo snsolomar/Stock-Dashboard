285f460e9ac6e743143122102ce6356d4430312a
snsolomar <snsolomar@gmail.com>
2023-08-04 14:54:54 -0500

    adjust grid column layout on main container to 2, and header md col span to 2, both adjusted from 3

--- Frontend/src/components/Dashboard.js
+++ Frontend/src/components/Dashboard.js
@@ -3,8 +3,8 @@ import Cards from './Cards'
 
 const Dashboard = () => {
   return (
-    <div className='h-screen grid grid-cols-1 md:grid-cols-3 xl:grids-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand'>
-      <div className='col-span-1 md:col-span-3 xl:col-span-3 row-span-1'>
+    <div className='h-screen grid grid-cols-1 md:grid-cols-2 xl:grids-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand'>
+      <div className='col-span-1 md:col-span-2 xl:col-span-3 row-span-1'>
         <Cards>Header</Cards>
         </div>
       <div className='md:col-span-2 row-span-4'>
