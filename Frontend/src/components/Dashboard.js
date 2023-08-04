e4c4c972011d0f94eac72777905f34da9138f69d
snsolomar <snsolomar@gmail.com>
2023-08-04 14:32:43 -0500

    Dashboard component css styled for responsive design

--- Frontend/src/components/Dashboard.js
+++ Frontend/src/components/Dashboard.js
@@ -3,8 +3,19 @@ import Cards from './Cards'
 
 const Dashboard = () => {
   return (
-    <div>
-      
+    <div className='h-screen grid grid-cols-1 md:grid-cols-2 xl:grids-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand'>
+      <div className='col-span-1 md:col-span-2 xl:col-span-3 row-span-1'>
+        <Cards>Header</Cards>
+        </div>
+      <div className='md:col-span-2 row-span-4'>
+        <Cards>Chart</Cards>
+        </div>
+      <div>
+        <Cards>Overview</Cards>
+        </div>
+      <div className='row-span-2 xl-row-span-3'>
+        <Cards>Details</Cards>
+        </div>
     </div>
   )
 }
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
       fontFamily: {
         quicksand: ["Quicksand", "sans-serif"],
       },
+      gridTemplateRows: { // Tailwind provides 6 rows for it's grid layout, so this extends it to 8
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+      },
     },
   },
   plugins: [],
