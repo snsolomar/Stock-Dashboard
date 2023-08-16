89d977a3de0b7f4a5b4090efd809bf0b7dc93ee1
snsolomar <snsolomar@gmail.com>
2023-08-16 00:37:56 -0500

    added color filter to change and changepercentage span to show green if positive and red if negative

--- Frontend/src/components/Overview.js
+++ Frontend/src/components/Overview.js
@@ -8,7 +8,7 @@ const Overview = ({ symbol, price, change, changePercent, currency }) => {
   return (
     <Cards>
         <span className='absolute left-4 top-4 text-neutral-500 text-lg xl:text-xl 2xl:text-2xl'>{symbol}</span>
-        <div className='w-full h-full flex items-center justify-around'>
+        <div className='w-full h-full flex items-center justify-around pt-5'>
             <span className='text-2xl xl:text-4xl 2xl:text-2xl text-neutral-500 m-2'>
                 {parseFloat(price).toFixed(2)}
                 <span className='text-lg xl:text-xl 2xl:text-2xl text-neutral-500 m-2'>
