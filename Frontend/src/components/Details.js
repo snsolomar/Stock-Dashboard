ebb16aa44c7cb78db8ee2abd3a1ac9a7bf35a02e
snsolomar <snsolomar@gmail.com>
2023-08-17 13:28:33 -0500

    conditional statement to check if value is a number, then apply commas where necesarry using toLocaleString()

--- Frontend/src/components/Details.js
+++ Frontend/src/components/Details.js
@@ -27,15 +27,23 @@ const Details = ({ details, summary }) => {
          <Cards>
             <ul className='w-full h-full flex flex-col justify-between divide-y-1'>
                {Object.keys(companySummary).map((item) => {
-                   return (
-                   <li 
-                   key={item} 
-                   className="flex-1 flex justify-between items-center"
-                   >
-                      <span>{item}:</span>
-                      <span>{companySummary[item]}</span>
-                   </li>
-                   )
+
+                  let value = companySummary[item];
+
+                  if (!isNaN(parseFloat(value))) {
+                     if (item === "Volume") {
+                           value = parseInt(value, 10).toLocaleString();
+                     } else {
+                           value = parseFloat(value).toFixed(2);
+                     }
+                     value = value.toLocaleString(); 
+                  }
+                  return (
+                     <li key={item} className="flex-1 flex justify-between items-center">
+                        <span>{item}:</span>
+                        <span>{isNaN(parseFloat(value)) ? companySummary[item] : value}</span>
+                     </li>
+                     );
                })}
             </ul>
          </Cards>
