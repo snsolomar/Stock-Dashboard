2607355b0e90a05877b5e9686d7eac11aa96c0bd
snsolomar <snsolomar@gmail.com>
2023-08-17 15:34:08 -0500

    CreateDate function created

---
+++ Frontend/src/utils/CreateDate.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CreateDate = (date, days, weeks, months, years) => {
+  let newDate = new Date(date)
+  newDate.setDate(newDate.getDate() + days + 7 * weeks);
+  newDate.setMonth(newDate.getMonth() + months);
+  newDate.setFullYear(newDate.getFullYear() + years);
+};
+
+export default CreateDate
